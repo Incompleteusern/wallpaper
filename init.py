3cf62b2f940aed1e699ffec3d68e37ba8f893ea9
royce <royce.yao.ry@Gmail.com>
2023-02-15 18:31:55 -0700

    cleanup

--- init.py
+++ init.py
@@ -1,10 +1,7 @@
 from pathlib import Path
-
-source_dir = Path(__file__).resolve().parent
-
 import os
 
-os.makedirs(os.path.join(source_dir, "wallpapers"))
+source_dir = Path(__file__).resolve().parent
 
 for i in ["Clear", "Snow", "Rain", "Clouds"]:
     for j in ["Morning", "Noon", "Night", "Evening"]:
